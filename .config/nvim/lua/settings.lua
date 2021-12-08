556c6e2871fc016dfcf6013bbd954918c25a1b5d
James Robert Rooke <thepoltergeist23@gmail.com>
2021-12-09 02:33:26 +0800

    disable prettier on js, jsx, ts, and tsx files

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -174,7 +174,10 @@ null_ls.config({
 	sources = {
 		null_ls.builtins.diagnostics.eslint_d, -- eslint or eslint_d
 		null_ls.builtins.code_actions.eslint_d, -- eslint or eslint_d
-		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.eslint_d,
+		null_ls.builtins.formatting.prettier.with({
+			disabled_filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+		}),
 		null_ls.builtins.formatting.stylua,
 	},
 })
