174ed3df205410a19ae34ffa8e81aebe463af4f5
James Robert Rooke <thepoltergeist23@gmail.com>
2022-02-18 00:56:06 +0800

    remove autocmd in options.lua

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -72,22 +72,3 @@ end
 
 load_defaults()
 cmd([[colorscheme tokyonight]])
-
--- Highlight on yank
-api.nvim_exec(
-	[[
-    augroup YankHighlight
-      autocmd!
-      autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-    augroup end
-  ]],
-	false
-)
-
--- Autoread
-api.nvim_exec(
-	[[
-  autocmd FocusGained,BufEnter * :checktime
-]],
-	false
-)
