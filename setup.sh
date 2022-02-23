b4a0954f3ee995c419e2a4c920041a089cd5cf3b
James Robert Rooke <thepoltergeist23@gmail.com>
2022-02-24 00:14:41 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,46 +8,52 @@ yellow=$(tput setaf 3);
 green=$(tput setaf 2);
 clear=$(tput sgr0);
 
-echo "${green}Update/Upgrade system${clear}"
+echo "${green}Update/Upgrade system...${clear}"
 sudo apt update && sudo apt dist-upgrade -y
 
 echo "${green}Installing Build Essential and more...${clear}"
 sudo apt install build-essential curl wget git zsh -y
 
-echo "${green}Installing Archive Utilities${clear}"
+echo "${green}Installing Archive Utilities...${clear}"
 sudo apt install rar unrar p7zip-full p7zip-rar -y
 
-echo "${green}Installing HomeBrew${clear}"
+echo "${green}Installing HomeBrew...${clear}"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/jamespotz/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-echo "${green}Installing essential packages${clear}"
-brew install yadm exa ripgrep neovim fnm zsh-syntax-highlighting zsh-autosuggestions bat git tree-sitter luajit starship
+echo "${green}Installing essential homebrew packages...${clear}"
+brew install yadm exa ripgrep neovim fnm zsh-syntax-highlighting zsh-autosuggestions bat tree-sitter luajit starship zoxide
 
-echo "${green}ZSH setup${clear}"
+echo "${green}ZSH setup...${clear}"
 mv ~/.zshrc ~/zshrc.bak
 sudo chsh -s "$(which zsh)" "${USER}"
 
-echo "${green}Setup ssh keygen${clear}"
+echo "${green}Setup ssh keygen...${clear}"
 echo "Email:"
 read -r email
 yes "" | ssh-keygen -t rsa -C "${email}"
-echo "${green}Copy the ssh key below and add to Github/Bitbucket account:${clear}"
-"${yellow}"; cat "${HOME}"/.ssh/id_rsa.pub; "${clear}"
+echo "${green}Copy the ssh key below and add to Github/Bitbucket account${clear}"
+cat "${HOME}/.ssh/id_rsa.pub"
 
 echo "${green}Press [SPACE] to continue...${clear}"
 read -r -s -d ' '
 
-echo "${green}Creating work directory${clear}"
-mkdir -p "${HOME}"/Work
+echo "${green}Creating work directory...${clear}"
+mkdir -p "${HOME}/Work"
 
-echo "${green}Cloning config${clear}"
+echo "${green}Cloning config...${clear}"
 yadm clone git@github.com:jamespotz/config.git
 
-echo "${green}Installing neovim packages${clear}"
+echo "${green}Installing neovim packages...${clear}"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.neovim
 nvim --headless +PackerUpdate +qall
 
-echo "${green}DONE...${clear}"
+echo "${green}Installing node v14...${clear}"
+fnm install v14
+
+echo "${green}Installing global npm packages eslint_d, prettierd, and yarn...${clear}"
+npm install -g yarn eslint_d @fsouza/prettierd
+
+echo "${green}DONE!🚀🚀${clear}"
