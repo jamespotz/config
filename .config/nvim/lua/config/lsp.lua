9bf5fd4efe90668264f1ffeb5d0fc00c6258b195
James Robert Rooke <thepoltergeist23@gmail.com>
2022-07-26 01:20:05 +0800

    mason icons

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -111,9 +111,9 @@ local configurations = {
 mason.setup({
 	ui = {
 		icons = {
-			server_installed = "✓",
-			server_pending = "➜",
-			server_uninstalled = "✗",
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
 		},
 	},
 })
