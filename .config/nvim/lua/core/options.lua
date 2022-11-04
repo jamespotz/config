0d10f4f2fa041df68b8ef9a086c68951bd21493c
James Robert Rooke <thepoltergeist23@gmail.com>
2022-11-04 13:21:28 +0800

    update config

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -36,62 +36,70 @@ end
 
 local load_defaults = function()
 	local default_options = {
+		background = "dark",
 		backspace = { "indent", "eol", "start" },
 		backup = false, -- creates a backup file
 		clipboard = "unnamedplus", -- allows neovim to access the system clipboard
 		cmdheight = 2, -- more space in the neovim command line for displaying messages
 		colorcolumn = "80",
 		conceallevel = 0, -- so that `` is visible in markdown files
+		cursorline = true, -- highlight the current line
+		expandtab = true, -- convert tabs to spaces
 		fileencoding = "utf-8", -- the encoding written to a file
-		foldmethod = "manual", -- folding, set to "expr" for treesitter based folding
 		foldexpr = "", -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
+		foldmethod = "manual", -- folding, set to "expr" for treesitter based folding
+		guifont = "VictorMono NF:h12:ub",
 		hidden = true, -- required to keep multiple buffers and open multiple buffers
 		hlsearch = true, -- highlight all matches on previous search pattern
 		ignorecase = true, -- ignore case in search patterns
+		inccommand = "nosplit", -- Show incremental live substitution
+		lazyredraw = false, -- Only redraw when necessary
+		list = true,
 		mouse = "a", -- allow the mouse to be used in neovim
+		number = true, -- set numbered lines
+		numberwidth = 4, -- set number column width to 2 {default 4}
+		pumblend = 5, -- pop up menu transparency
 		pumheight = 10, -- pop up menu height
-		pumblend = 20, -- pop up menu transparency
-		winblend = 20, -- floating window transparency
+		relativenumber = true, -- set relative numbered lines
+		scrolloff = 8,
+		shell = "/usr/bin/zsh",
+		shiftwidth = 2, -- the number of spaces inserted for each indentation
 		showmode = false, -- we don't need to see things like -- INSERT -- anymore
 		showtabline = 2, -- always show tabs
+		sidescrolloff = 8,
+		signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
 		smartcase = true, -- smart case
 		smartindent = true, -- make indenting smarter again
+		spell = true,
+		spelllang = { "en_us" },
 		splitbelow = true, -- force all horizontal splits to go below current window
 		splitright = true, -- force all vertical splits to go to the right of current window
 		swapfile = false, -- creates a swapfile
+		tabstop = 2, -- insert 2 spaces for a tab
 		termguicolors = true, -- set term gui colors (most terminals support this)
+		textwidth = 80,
 		title = true, -- set the title of window to the value of the titlestring
 		undodir = undo_dir, -- set the undo directory to be saved
 		undofile = true, -- enable persistent undo
 		updatetime = 250, -- faster completion
-		writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-		expandtab = true, -- convert tabs to spaces
-		shiftwidth = 2, -- the number of spaces inserted for each indentation
-		tabstop = 2, -- insert 2 spaces for a tab
-		cursorline = true, -- highlight the current line
-		number = true, -- set numbered lines
-		relativenumber = true, -- set relative numbered lines
-		numberwidth = 4, -- set number column width to 2 {default 4}
-		signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-		wrap = false, -- display lines as one long line
-		spell = true,
-		spelllang = { "en_us" },
-		scrolloff = 8,
-		sidescrolloff = 8,
+		wildcharm = fn.char2nr("^I"), -- tab completion for the wildmenu
 		wildmenu = true,
 		wildmode = "full",
-		wildcharm = fn.char2nr("^I"), -- tab completion for the wildmenu
-		lazyredraw = false, -- Only redraw when necessary
-		inccommand = "nosplit", -- Show incremental live substitution
-		textwidth = 80,
-		shell = "/usr/bin/zsh",
-		list = true,
-		guifont = "VictorMono NF:h12:b",
+		winblend = 0, -- floating window transparency
+		wrap = false, -- display lines as one long line
+		writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+		wildoptions = "pum",
 	}
 
 	---  SETTINGS  ---
-
 	opt.shortmess:append("c")
+
+	-- Add asterisks in block comments
+	opt.formatoptions:append({ "r" })
+
+	opt.wildignore:append({ "*/node_modules/*" })
+	opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+
 	--[[ opt.listchars:append({ eol = "↴", space = "⋅" }) ]]
 
 	for k, v in pairs(default_options) do
@@ -100,6 +108,16 @@ local load_defaults = function()
 end
 
 load_defaults()
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+	pattern = "*",
+	command = "set nopaste",
+})
 
 -- Neovide config
 vim.g.neovide_refresh_rate = 144
