22bfe16fa89b0480b9e40e1f58cf8b2b5fe4c1a2
James Robert Rooke <thepoltergeist23@gmail.com>
2023-01-05 06:13:14 +0800

    add jump back to last cursor position

--- .config/nvim/lua/core/autocmd.lua
+++ .config/nvim/lua/core/autocmd.lua
@@ -19,3 +19,13 @@ api.nvim_create_autocmd({ "WinEnter", "BufEnter", "FocusGained" }, {
 	pattern = "*",
 	command = "silent! checktime",
 })
+
+api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = api.nvim_buf_get_mark(0, '"')
+		local lcount = api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
