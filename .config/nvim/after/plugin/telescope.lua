5455b8c2a8bd9da47ddf9444d993f0a5af5e8c18
James Robert Rooke <thepoltergeist23@gmail.com>
2023-02-01 03:49:18 +0800

    update config

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -43,6 +43,7 @@ telescope.setup({
 		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
 		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
 		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+		initial_mode = "normal",
 
 		mappings = {
 			i = {
